08ac3df00b600eeb72cf13e0ba1178a508bb8554
David Wheatley <hi@davwheat.dev>
2022-09-20 22:38:10 +0100

    feat: link to docs in api payload info

--- wrapResponse.js
+++ wrapResponse.js
@@ -3,6 +3,7 @@ module.exports = function wrapResponse(response, metadata = { ok: true }) {
     info: [
       'This service is provided free-of-charge (for now), courtesy of dav.network.',
       'If you use this a lot, please consider supporting me: https://github.com/sponsors/davwheat',
+      'Documenation is found at https://github.com/davwheat/mastdatabase-api-proxy-services#readme',
       'Anyway, the raw data from the API you wanted is found below! Enjoy!',
     ],
     ...metadata,
