146e9b49de6d140fd37dce42c364960a485c7563
David Wheatley <david.wheatley@raileasy.co.uk>
2023-12-07 10:36:51 +0100

    feat: update proxy to match new three endpoint

--- src/routes/uk/three/ran-status/index.ts
+++ src/routes/uk/three/ran-status/index.ts
@@ -1,32 +1,18 @@
 import wrapResponse from '../../../../wrapResponse.js';
 
 import threeOutagesRequest from './threeOutagesRequest.js';
-import threeCoverageRequest from './threeCoverageRequest.js';
-import threeHbbRequest from './threeHbbRequest.js';
 
 import type { FastifyInstance, FastifyServerOptions } from 'fastify';
 import type { FromSchema } from 'json-schema-to-ts';
 
-const handlers = {
-  outages: threeOutagesRequest,
-  coverage: threeCoverageRequest,
-  hbb: threeHbbRequest,
-} as const;
-
-const endpoints = Object.keys(handlers) as any as keyof typeof handlers;
-
 const QSType = {
   type: 'object',
-  required: ['endpoint', 'postcode'],
+  required: ['postcode'],
   properties: {
     postcode: {
       type: 'string',
       minLength: 1,
       maxLength: 16,
-    },
-    endpoint: {
-      type: 'string',
-      enum: endpoints,
     },
   },
 } as const;
@@ -39,9 +25,9 @@ export default async function (fastify: FastifyInstance, opts?: FastifyServerOpt
       querystring: QSType,
     },
     handler: async function (request, reply) {
-      const { postcode, endpoint } = request.query;
+      const { postcode } = request.query;
 
-      const data = (await handlers[endpoint as keyof typeof handlers](postcode)) as any;
+      const data: any = await threeOutagesRequest(postcode);
 
       reply.send(wrapResponse(data));
     },
--- src/routes/uk/three/ran-status/threeOutagesRequest.ts
+++ src/routes/uk/three/ran-status/threeOutagesRequest.ts
@@ -1,14 +1,35 @@
 import fetch from 'node-fetch';
 
 export default async function threeOutagesRequest(postcode: string) {
-  const params = new URLSearchParams({
-    postcode,
-    content: 'true',
+  const locationData = await fetch(`https://api.postcodes.io/postcodes/${encodeURIComponent(postcode)}`, {
+    headers: {
+      'User-Agent': `mastdatabase.co.uk backend (contact: david@mastdatabase.co.uk)`,
+    },
   });
 
-  const data = await fetch(`https://www.three.co.uk/rig/outages?${params}`, {
+  if (!locationData.ok) {
+    return {
+      error: 'Unknown postcode or error fetching location information',
+      detail: `${locationData.status} ${locationData.statusText}`,
+      upstreamBody: await locationData.text(),
+    };
+  }
+
+  const locationDataResponse: any = await locationData.json();
+
+  const { latitude, longitude } = locationDataResponse.result;
+
+  const data = await fetch(`https://www.three.co.uk/bin/threedigital/rigview`, {
+    method: 'POST',
+    body: JSON.stringify({
+      location: { postcode, latitude, longitude, precisionLocation: 'APPROXIMATE' },
+      ClientID: 'STATUSCHECKER',
+    }),
     headers: {
-      Referer: 'https://www.three.co.uk/support/network_and_coverage/network_support',
+      Accept: '*/*',
+      'Content-Type': 'application/json',
+      Referer: 'https://www.three.co.uk/support/network-and-coverage/network-support?intid=coveragecheckpage-button-checknetworkissues',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
     },
   });
 
